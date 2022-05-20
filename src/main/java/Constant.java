e7ebe3223fceb9252b02117a0cdd2414bca7783b
AayushiGandhi <ay753882@dal.ca>
2022-05-20 12:49:16 -0300

    Aws S3 initial setup

---
+++ src/main/java/Constant.java
@@ -0,0 +1,5 @@
+public class Constant {
+    public static final String aws_access_key_id = "ASIATF45ZMJ5TFCU44VQ";
+    public static final String aws_secret_access_key = "KgwBdXtBLAwMmdIeznntljxYRZ9lTvApG2rUF2Qf";
+    public static final String aws_session_token = "FwoGZXIvYXdzEEEaDI3oU9lfxOphAChgeyLAAVu+vJbvVM57Bnn7JOJKvAvrkWyhmZxmPLCUic5p2qTuxvHcaQDi0vH45vlunQzCHQ7g090uwhcoNOR98qIjR4UOzAdI24vI9Zdb4uI7Lox3TvajAOzqqWJyjLGxHDsK7U/TbRRsPe3tM69706xOv6OGODlhnVDD9MwoR8NKDo/4hpyudIkSph9b1q0Mf+YiO+lRN6JYyJngAH+bF+QZWxs/PuMWFOawptpAdqySIT7XIe5l9UHAKtCuvFb+KmQiuSiI5p6UBjItqWh9CtOseEx/XaEh/A8MaVYowIGwuZ6mlPH5I8x9Z//A5r3DTCpW7xEetbjE";
+}
---
+++ src/main/java/S3_Experiment.java
@@ -0,0 +1,12 @@
+
+
+import java.util.List;
+
+public class S3_Experiment {
+    public void createS3Bucket(){
+    }
+
+    public void uploadFileToS3Bucket(){
+
+    }
+}
